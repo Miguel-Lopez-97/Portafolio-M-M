da1842d1b0bb674fcfb80933ea411b5ba2ed20d1
Miguel-Lopez-97 <miguel.m.lopez.p@gmail.com>
2021-12-20 04:38:08 -0500

    JS - CSS Timeline button execution configuration with graph change

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,13 @@
 //
 let FPS = 60;
 let numberChange = 1;
+let number = 1;
 
 function mainCarousel() {
   let imagenMainCarosusel = document.getElementById("introduce_carousel_image");
   this.changeRuteImage = "media/images/Diapositiva" + numberChange + ".PNG";
   imagenMainCarosusel.src = this.changeRuteImage;
+  console.log(imagenMainCarosusel.src)
 }
 
 let carousel1 = function () {
@@ -21,25 +23,41 @@ let carousel3 = function () {
   mainCarousel();
 };
 
+function timeLine() {
+    let changeimagen = document.getElementById("timeLine_graph");
+  this.changeRuteImage = "media/images/Time_line" + number + ".PNG";
+  changeimagen.src = this.changeRuteImage;
+  console.log(changeimagen.src)
+  document.getElementById("timeLineDiv").style.display= "flex"
+  }
+
+function timeLineClose(){
+    document.getElementById("timeLineDiv").style.display= "none";
+}
+
+let timeLineDevelopment = function () {
+    number=1
+    timeLine()
+  }
+
+let timeLineEngineering = function () {
+    number=2
+    timeLine()
+  }
 function time() {
   let timeNow = new Date();
   this.segNow = Math.floor(timeNow.getSeconds() / 2);
   console.log(numberChange,segNow);
-  if (segNow == 29 && numberChange < 4) {
-    for(i=0;i<1;i++){
+  if (segNow == 29 && numberChange < 3) {
         numberChange++;
         mainCarousel();}
-    
-    console.log(numberChange);
-  }
-  else if(numberChange > 3){
+  else if(segNow == 29 &&numberChange == 3){
     numberChange = 1;
     mainCarousel();
   }
 }
 function principal() {
   mainCarousel();
-  numberChange=1;
 }
 function init() {
   principal();
