f1c55ebc8bdee7c0c9278388471af0672abe88d5
Miguel-Lopez-97 <miguel.m.lopez.p@gmail.com>
2021-12-20 11:28:28 -0500

    CSS & JS -the execution of Carousel buttons Professional change and move the carousel

--- scripts/app.js
+++ scripts/app.js
@@ -83,31 +83,76 @@ function time() {
 }
 
 function professionalMainCarousel(){
-    document.getElementById("prof_main_name_-1").innerHTML="WandaVision"
-    document.getElementById("prof_main_key_-1").innerHTML="CSS-Landing page Nov-21"
-    document.getElementById("prof_main_img_-1").src="media/images/Prof_Carousel/prof_img_main_1.png"
-    document.getElementById("prof_main_text_-1").innerHTML="Wanda Maximoff and Vision are living an idyllic suburban life in the town of Westview, New Jersey, trying to conceal their true natures."
+    this.dataMatrix = [
+        [["WandaVision"],["CSS-Landing page Nov-21"],["https://www.disneyplus.com/series/wandavision/4SrN28ZjDLwH?irclickid=xoOQ8v1X-xyIT3K34FT2OQBMUkGwGI2P-0qRTU0&irgwc=1&cid=DSS-Affiliate-Impact-Content-JustWatch%20GmbH-705874"],["media/images/Prof_Carousel/prof_img_main_1.png"],["Wanda Maximoff and Vision are living an idyllic suburban life in the town of Westview, New Jersey, trying to conceal their true natures."],["https://www.justwatch.com/co/serie/bruja-escarlata-y-vision"]],
+        [["Portfolio M&M"],["Business page Nov-21"],["https://miguelmorenoportafolio.netlify.app/"],["media/images/Prof_Carousel/prof_img_main_2.png"],["This is a main page about my work as Web Development"],["https://github.com/Miguel-Lopez-97/Portafolio-M-M"]],
+        [["Jurassic Park"],["Landing page Nov-21"],["https://sleepy-archimedes-03d4ce.netlify.app/"],["media/images/Prof_Carousel/prof_img_main_0.png"],["This is a tribute page about my favorite saga in all the time"],["https://github.com/Miguel-Lopez-97/Landing-page-Jurassic-World-Novedades"]],
+        [["Hangman Game"],["Game page with JS Nov-21"],["https://juegoahorcadodiplo99.netlify.app"],["media/images/Prof_Carousel/prof_img_main_3.png"],["Hangman games for home This project is a hangman game developed for the home games company"],["https://github.com/Miguel-Lopez-97/Juego-del-Ahorcado"]]
+    ]
+    this.x=0
+    this.y=1
+    this.z=2
 
-    document.getElementById("prof_main_name_1").innerHTML="Portafolio M&M"
-    document.getElementById("prof_main_key_1").innerHTML="Business page Nov-21"
-    document.getElementById("prof_main_img_1").src="media/images/Prof_Carousel/prof_img_main_2.png"
-    document.getElementById("prof_main_text_1").innerHTML="This is a main page about my work as Web Development"
+    this.cargeMatrixProfessional = function(x,y,z){
+    document.getElementById("prof_main_name_-1").innerHTML=this.dataMatrix[x][0]
+    document.getElementById("prof_main_key_-1").innerHTML=this.dataMatrix[x][1]
+    document.getElementById("prof_main_img_-1").src=this.dataMatrix[x][3]
+    document.getElementById("prof_main_text_-1").innerHTML=this.dataMatrix[x][4]
 
-    document.getElementById("prof_main_name_0").innerHTML="Jurassic Park"
-    document.getElementById("prof_main_key_0").innerHTML="Landing page Nov-21"
-    document.getElementById("prof_main_img_link_0").href="https://sleepy-archimedes-03d4ce.netlify.app/"
-    document.getElementById("prof_main_img_0").src="media/images/Prof_Carousel/prof_img_main_0.png"
-    document.getElementById("prof_main_text_0").innerHTML="This is a tribute page about my favorite saga in all the time"
-    document.getElementById("prof_main_img_github_0").href="https://github.com/Miguel-Lopez-97/Landing-page-Jurassic-World-Novedades"
-                                        
-
+    document.getElementById("prof_main_name_0").innerHTML=this.dataMatrix[y][0]
+    document.getElementById("prof_main_key_0").innerHTML=this.dataMatrix[y][1]
+    document.getElementById("prof_main_img_link_0").href=this.dataMatrix[y][2]
+    document.getElementById("prof_main_img_0").src=this.dataMatrix[y][3]
+    document.getElementById("prof_main_text_0").innerHTML=this.dataMatrix[y][4]
+    document.getElementById("prof_main_img_github_0").href=this.dataMatrix[y][5]
+    
+    document.getElementById("prof_main_name_1").innerHTML=this.dataMatrix[z][0]
+    document.getElementById("prof_main_key_1").innerHTML=this.dataMatrix[z][1]
+    document.getElementById("prof_main_img_1").src=this.dataMatrix[z][3]
+    document.getElementById("prof_main_text_1").innerHTML=this.dataMatrix[z][4]
 }
+    this.changeMatrixProfessionalRigth = function(){
+        if(this.x==0){
+            this.x=this.dataMatrix.length-1
+        }
+        else{this.x--}
+        if(this.y==0){
+            this.y=this.dataMatrix.length-1
+        }
+        else{this.y--}
+        if(this.z==0){
+            this.z=this.dataMatrix.length-1
+        }
+        else{this.z--}
+        this.cargeMatrixProfessional(this.x,this.y,this.z)
+    }
+    this.changeMatrixProfessionalLeft = function(){
+        if(this.x==this.dataMatrix.length-1){
+            this.x=0
+        }
+        else{this.x++
+        console.log(this.x,this.y,this.z)}
+        if(this.y==this.dataMatrix.length-1){
+            this.y=0
+        }
+        else{this.y++}
+        if(this.z==this.dataMatrix.length-1){
+            this.z=0
+        }
+        else{this.z++}
+        this.cargeMatrixProfessional(this.x,this.y,this.z)
+    }
+}
+
+let player;
 
 function principal() {
   mainCarousel();
-  professionalMainCarousel()
+  professionalMainCarousel();
+  player.cargeMatrixProfessional(0,1,2);
 }
 function init() {
+    player = new professionalMainCarousel()
   principal();
   setInterval(function () {
     time();
